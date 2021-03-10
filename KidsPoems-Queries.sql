c0e6db1c672f3a768982a8529c896a9ef997814c
Rickie Le <rickienthefishies@gmail.com>
2021-03-10 12:25:04 -0600

    initial commit

---
+++ KidsPoems-Queries.sql
@@ -0,0 +1,22 @@
+﻿/*
+What grades are stored in the database?
+What emotions may be associated with a poem?
+How many poems are in the database?
+Sort authors alphabetically by name. What are the names of the top 76 authors?
+Starting with the above query, add the grade of each of the authors.
+Starting with the above query, add the recorded gender of each of the authors.
+What is the total number of words in all poems in the database?
+Which poem has the fewest characters?
+How many authors are in the third grade?
+How many authors are in the first, second or third grades?
+What is the total number of poems written by fourth graders?
+How many poems are there per grade?
+How many authors are in each grade? (Order your results by grade starting with 1st Grade)
+What is the title of the poem that has the most words?
+Which author(s) have the most poems? (Remember authors can have the same name.)
+How many poems have an emotion of sadness?
+How many poems are not associated with any emotion?
+Which emotion is associated with the least number of poems?
+Which grade has the largest number of poems with an emotion of joy?
+Which gender has the least number of poems with an emotion of fear?
+*/
